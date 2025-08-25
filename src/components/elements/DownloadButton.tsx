7bdc4921a69d29a68c1ba16b33dcdef62a0baaf6
Immanuel Onum <immanuel.o.onum@gmail.com>
2025-08-25 13:18:06 +0100

    update download button style

--- src/components/elements/DownloadButton.tsx
+++ src/components/elements/DownloadButton.tsx
@@ -3,11 +3,9 @@ import ArrowIcon from './lcons/ArrowIcon'
 
 const DownloadButton = () => {
   return (
-    <button className='rounded-full border-2 border-[#6F126D]'>
-        <span className='w-full border-2 border-[#D85CC5] px-[22px] py-[14px] rounded-full flex items-center justify-center gap-x-[6px] font-[600] leading-[24px] text-white bg-gradient-to-b from-[#D85CC5] to-[#FF3FEC]'>
-            Download Now
-            <ArrowIcon className='text-white w-[16px] h-[16px] -rotate-90' />
-        </span>
+    <button className='w-full px-[22px] py-[14px] rounded-full flex items-center justify-center gap-x-[6px] font-[600] leading-[24px] text-white bg-[#B001B0]'>
+      Download Now
+      <ArrowIcon className='text-white w-[16px] h-[16px] -rotate-90' />
     </button>
   )
 }
